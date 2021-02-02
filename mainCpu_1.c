9c099bd8d8eb9ad708fa517be1b472f310e81fbd
mvcjhkim <mvcjhkim@gmail.com>
2021-02-02 15:52:36 +0900

    ADC BURST MODE COMPLETE AGAIN

--- mainCpu_1.c
+++ mainCpu_1.c
@@ -1,64 +1,6 @@
-//#############################################################################
-//
-// FILE:   adc_ex2_soc_epwm.c
-//
-// TITLE:  ADC ePWM Triggering
-//
-//! \addtogroup driver_example_list
-//! <h1>ADC ePWM Triggering</h1>
-//!
-//! This example sets up ePWM1 to periodically trigger a conversion on ADCA.
-//!
-//! \b External \b Connections \n
-//!  - A0 should be connected to a signal to convert
-//!
-//! \b Watch \b Variables \n
-//! - \b adcAResults_1 - A sequence of analog-to-digital conversion samples from
-//!   pin A0. The time between samples is determined based on the period
-//!   of the ePWM timer.
-//!
-//
-//#############################################################################
-// $TI Release: F2837xD Support Library v3.11.00.00 $
-// $Release Date: Sun Oct  4 15:55:24 IST 2020 $
-// $Copyright:
-// Copyright (C) 2013-2020 Texas Instruments Incorporated - http://www.ti.com/
-//
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions 
-// are met:
-// 
-//   Redistributions of source code must retain the above copyright 
-//   notice, this list of conditions and the following disclaimer.
-// 
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the 
-//   documentation and/or other materials provided with the   
-//   distribution.
-// 
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// $
-//#############################################################################
-
-//
-// Included Files
-//
 #include "driverlib.h"
 #include "device.h"
+#include "F2837xD_device.h"
 
 // Defines
 #define CPU_FRQ_200MHZ          1
@@ -236,6 +178,7 @@ void fft_routine()
 
 #define BLINKY_LED_GPIO 31
 #define PULSE_OUTPUT_GPIO 18
+#define GPIO_0     0
 uint16_t ToggleCount = 0;
 void initLocalGpio()
 {
@@ -248,6 +191,9 @@ void initLocalGpio()
     GPIO_setPadConfig(PULSE_OUTPUT_GPIO , GPIO_PIN_TYPE_STD);
     GPIO_setMasterCore(PULSE_OUTPUT_GPIO , GPIO_CORE_CPU1);
     GPIO_setQualificationMode(PULSE_OUTPUT_GPIO , GPIO_QUAL_SYNC);
+
+    GPIO_setPadConfig(GPIO_0, GPIO_PIN_TYPE_STD);
+    GPIO_setPinConfig(GPIO_0_EPWM1A);
 }
 
 inline void initBuffer()
@@ -340,11 +286,12 @@ void main(void)
 //
 void initADC(void)
 {
+    /*
     ADC_setPrescaler(ADCA_BASE, ADC_CLK_DIV_4_0); // Set ADCCLK divider to /4
     ADC_setMode(ADCA_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED); // // Set resolution and signal mode (see #defines above) and load // corresponding trims.
     ADC_setInterruptPulseMode(ADCA_BASE, ADC_PULSE_END_OF_CONV); // Set pulse positions to late
     ADC_enableConverter(ADCA_BASE); // Power up the ADC and then delay for 1 ms
-    /*
+     */
     int xx;
     for(xx=0;xx<4*0x80;xx+=0x80){
         ADC_setPrescaler(ADCA_BASE+xx, ADC_CLK_DIV_4_0); // Set ADCCLK divider to /4
@@ -352,7 +299,6 @@ void initADC(void)
         ADC_setInterruptPulseMode(ADCA_BASE+xx, ADC_PULSE_END_OF_CONV); // Set pulse positions to late
         ADC_enableConverter(ADCA_BASE+xx); // Power up the ADC and then delay for 1 ms
     };
-     */
     DEVICE_DELAY_US(1000);
 }
 
@@ -364,22 +310,9 @@ void initEPWM(void)
     EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A); // Disable SOCA
     EPWM_setADCTriggerSource(EPWM1_BASE,EPWM_SOC_A,EPWM_SOC_TBCTR_PERIOD);
     EPWM_setADCTriggerEventPrescale(EPWM1_BASE, EPWM_SOC_A, 1);
-    EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 0x0800);
-    EPWM_setTimeBasePeriod(EPWM1_BASE, 0x1000);
+    //EPWM_setCountrCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 0x0800);
+    EPWM_setTimeBasePeriod(EPWM1_BASE, 0x1000);//EPWM_setTimeBasePeriod(EPWM1_BASE, 0x30d4);//At 8Khz
     EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE);
-    //
-    //EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE); // Freeze the counter
-    //EPWM_setClockPrescaler(EPWM1_BASE, EPWM_CLOCK_DIVIDER_1 ,EPWM_HSCLOCK_DIVIDER_1 );
-    //EPWM_setTimeBasePeriod(EPWM1_BASE, 0x30d4);//At 8Khz
-    //EPWM_setInterruptEventCount(EPWM1_BASE, 1);
-
-    //
-    //EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE); // Freeze the counter
-    //EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A); // Disable SOCA
-    //EPWM_setADCTriggerSource(EPWM1_BASE, EPWM_SOC_A, EPWM_SOC_TBCTR_U_CMPA); // Configure the SOC to occur on the first up-count event
-    //EPWM_setADCTriggerEventPrescale(EPWM1_BASE, EPWM_SOC_A, 1);
-    //EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 0x0800); // Set the compare A value to 2048 and the period to 4096
-    //EPWM_setTimeBasePeriod(EPWM1_BASE, 0x1000);
 }
 
     //EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_UP );
@@ -388,54 +321,73 @@ void initEPWM(void)
 //
 void initADCSOC(void)
 {
-
-    //
-    // Configure SOC0 of ADCA to convert pin A0. The EPWM1SOCA signal will be
-    // the trigger.
-    //
-    // For 12-bit resolution, a sampling window of 15 (75 ns at a 200MHz
-    // SYSCLK rate) will be used.  For 16-bit resolution, a sampling window of
-    // 64 (320 ns at a 200MHz SYSCLK rate) will be used.
-    //
+    //AdcaRegs.ADCCTL2.bit.PRESCALE = 6;
+    // For 12-bit resolution, a sampling window of 15 (75 ns at a 200MHz // SYSCLK rate) will be used.
     //ADCA_BASE
+    // max number 8 for adca, 8 * 44 sysclk cycle  352 * 5us = 1.762uS
        ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);//VIN_R
        ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN1, 15);//VSTS_R
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_R
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IIN_R
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER4, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//IINV_R
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER5, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN5, 15);//IO_R
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER14, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN14, 15);//THR1
-       //ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER15, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN15, 15);//THR2
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_R
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IIN_R
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER4, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//IINV_R
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER5, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN5, 15);//IO_R
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER6, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN14, 15);//THR1
+       ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER7, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN15, 15);//THR2
 
     //ADCB_BASE
-       //ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);//VIN_S
-       //ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN1, 15);//VSTS_S1
-       //ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_S
-       //ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IIN_S
+       ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);//VIN_S
+       ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN1, 15);//VSTS_S1
+       ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_S
+       ADC_setupSOC(ADCB_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IIN_S
 
     //ADCC_BASE
-       //ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_T
-       //ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IO_S
-       //ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER4, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//IINV_R
+       ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//VO_T
+       ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IO_S
+       ADC_setupSOC(ADCC_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//IINV_R
 
     //ADCD_BASE
-       //ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);//VBAT
-       //ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN1, 15);//VSTS_S
-       //ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//IBAT
-       //ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IO_T
-       //ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER4, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//THR2
+       ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);//VBAT
+       ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER1, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN1, 15);//VSTS_S
+       ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER2, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN2, 15);//IBAT
+       ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER3, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN3, 15);//IO_T
+       ADC_setupSOC(ADCD_BASE, ADC_SOC_NUMBER4, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN4, 15);//THR2
     //
     // Set SOC0 to set the interrupt 1 flag. Enable the interrupt and make
     // sure its flag is cleared.
-
-
-    ADC_setBurstModeConfig(ADCA_BASE,ADC_TRIGGER_EPWM1_SOCA , 2);
+    ADC_setBurstModeConfig(ADCA_BASE,ADC_TRIGGER_EPWM1_SOCA , 8);
+    ADC_setSOCPriority(ADCA_BASE,ADC_PRI_ALL_HIPRI);
     ADC_enableBurstMode(ADCA_BASE);
 
-    ADC_setInterruptSource(ADCA_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER1);//
+    ADC_setBurstModeConfig(ADCB_BASE,ADC_TRIGGER_EPWM1_SOCA , 4);
+    ADC_setSOCPriority(ADCB_BASE,ADC_PRI_ALL_HIPRI);
+    ADC_enableBurstMode(ADCB_BASE);
+
+    ADC_setBurstModeConfig(ADCC_BASE,ADC_TRIGGER_EPWM1_SOCA , 3);
+    ADC_setSOCPriority(ADCC_BASE,ADC_PRI_ALL_HIPRI);
+    ADC_enableBurstMode(ADCC_BASE);
+
+    ADC_setBurstModeConfig(ADCD_BASE,ADC_TRIGGER_EPWM1_SOCA , 5);
+    ADC_setSOCPriority(ADCD_BASE,ADC_PRI_ALL_HIPRI);
+    ADC_enableBurstMode(ADCD_BASE);
+    //ADCA
+    ADC_setInterruptSource(ADCA_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER7);//
     ADC_enableInterrupt(ADCA_BASE, ADC_INT_NUMBER1);
     ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
 
+    //ADCB
+    ADC_setInterruptSource(ADCB_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER3);//
+    ADC_enableInterrupt(ADCB_BASE, ADC_INT_NUMBER1);
+    ADC_clearInterruptStatus(ADCB_BASE, ADC_INT_NUMBER1);
+
+    //ADCC
+    ADC_setInterruptSource(ADCC_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER2);//
+    ADC_enableInterrupt(ADCC_BASE, ADC_INT_NUMBER1);
+    ADC_clearInterruptStatus(ADCB_BASE, ADC_INT_NUMBER1);
+
+    //ADCD
+    ADC_setInterruptSource(ADCD_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER4);//
+    ADC_enableInterrupt(ADCD_BASE, ADC_INT_NUMBER1);
+    ADC_clearInterruptStatus(ADCB_BASE, ADC_INT_NUMBER1);
 }
 
 //
@@ -443,12 +395,38 @@ void initADCSOC(void)
 //
 __interrupt void adcA1ISR(void)
 {
-    //
-    // Add the latest result to the buffer
-    //
-    adcAResults_1[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
-
-    adcAResults_2[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER1);
+    if( ADC_getInterruptStatus(ADCB_BASE,ADC_INT_NUMBER1)){
+        adcAResults_1[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
+        adcAResults_2[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER1);
+        adcAResults_3[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER2);
+        adcAResults_4[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER3);
+        adcAResults_5[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER4);
+        adcAResults_6[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER5);
+        adcAResults_7[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER6);
+        adcAResults_8[index] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER7);
+    }
+    else fail++;
+    if( ADC_getInterruptStatus(ADCB_BASE,ADC_INT_NUMBER1)){
+        adcAResults_9[index] = ADC_readResult(ADCBRESULT_BASE, ADC_SOC_NUMBER0);
+        adcAResults_10[index] = ADC_readResult(ADCBRESULT_BASE, ADC_SOC_NUMBER1);
+        adcAResults_11[index] = ADC_readResult(ADCBRESULT_BASE, ADC_SOC_NUMBER2);
+        adcAResults_12[index] = ADC_readResult(ADCBRESULT_BASE, ADC_SOC_NUMBER3);
+    }
+    else fail++;
+    if( ADC_getInterruptStatus(ADCC_BASE,ADC_INT_NUMBER1)){
+        adcAResults_13[index] = ADC_readResult(ADCCRESULT_BASE, ADC_SOC_NUMBER0);
+        adcAResults_14[index] = ADC_readResult(ADCCRESULT_BASE, ADC_SOC_NUMBER1);
+        adcAResults_15[index] = ADC_readResult(ADCCRESULT_BASE, ADC_SOC_NUMBER3);
+    }
+    else fail++;
+    if( ADC_getInterruptStatus(ADCD_BASE,ADC_INT_NUMBER1)){
+        adcAResults_16[index] = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER0);
+        adcAResults_17[index] = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER1);
+        adcAResults_18[index] = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER2);
+        adcAResults_19[index] = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER3);
+        adcAResults_20[index] = ADC_readResult(ADCDRESULT_BASE, ADC_SOC_NUMBER4);
+    }
+    else fail++;
     index++;
     // Trigger to soc 1
 
@@ -469,19 +447,94 @@ __interrupt void adcA1ISR(void)
     //
     // Clear the interrupt flag
     //
-    ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+    int xx;
+    for(xx=0;xx<4*0x80;xx+=0x80)
+        ADC_clearInterruptStatus(ADCA_BASE+xx, ADC_INT_NUMBER1);
 
     //
     // Check if overflow has occurred
     //
-    if(true == ADC_getInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1))
-    {
-        ADC_clearInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1);
-        ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+    for(xx=0;xx<4*0x80;xx+=0x80){
+        if(true == ADC_getInterruptOverflowStatus(ADCA_BASE+xx, ADC_INT_NUMBER1))
+        {
+            ADC_clearInterruptOverflowStatus(ADCA_BASE+xx, ADC_INT_NUMBER1);
+            ADC_clearInterruptStatus(ADCA_BASE+xx, ADC_INT_NUMBER1);
+        }
     }
 
     //
     // Acknowledge the interrupt
-    //
+    // ADCA1 and  ADCB1 and  ADCC1 and  ADCD1 are GROUP1
     Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
 }
+
+//#############################################################################
+//
+// FILE:   adc_ex2_soc_epwm.c
+//
+// TITLE:  ADC ePWM Triggering
+//
+//! \addtogroup driver_example_list
+//! <h1>ADC ePWM Triggering</h1>
+//!
+//! This example sets up ePWM1 to periodically trigger a conversion on ADCA.
+//!
+//! \b External \b Connections \n
+//!  - A0 should be connected to a signal to convert
+//!
+//! \b Watch \b Variables \n
+//! - \b adcAResults_1 - A sequence of analog-to-digital conversion samples from
+//!   pin A0. The time between samples is determined based on the period
+//!   of the ePWM timer.
+//!
+//
+//#############################################################################
+// $TI Release: F2837xD Support Library v3.11.00.00 $
+// $Release Date: Sun Oct  4 15:55:24 IST 2020 $
+// $Copyright:
+// Copyright (C) 2013-2020 Texas Instruments Incorporated - http://www.ti.com/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+//   Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the
+//   distribution.
+//
+//   Neither the name of Texas Instruments Incorporated nor the names of
+//   its contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// $
+//#############################################################################
+
+//
+// Included Files
+//
+    //
+    //EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE); // Freeze the counter
+    //EPWM_setClockPrescaler(EPWM1_BASE, EPWM_CLOCK_DIVIDER_1 ,EPWM_HSCLOCK_DIVIDER_1 );
+    //EPWM_setTimeBasePeriod(EPWM1_BASE, 0x30d4);//At 8Khz
+    //EPWM_setInterruptEventCount(EPWM1_BASE, 1);
+    //EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE); // Freeze the counter
+    //EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A); // Disable SOCA
+    //EPWM_setADCTriggerSource(EPWM1_BASE, EPWM_SOC_A, EPWM_SOC_TBCTR_U_CMPA); // Configure the SOC to occur on the first up-count event
+    //EPWM_setADCTriggerEventPrescale(EPWM1_BASE, EPWM_SOC_A, 1);
+    //EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 0x0800); // Set the compare A value to 2048 and the period to 4096
+    //EPWM_setTimeBasePeriod(EPWM1_BASE, 0x1000);
